93b6354446777d24fd03bf93f47ce32170f2a9c7
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-01-08 16:34:12 +0100

    Modification function(s).php + ajout register nav_menus

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+// Menu 
+register_nav_menus( array(
+    'megamenu' => 'Mega Menu',
+	  'main' => 'Menu Principal',
+	  'footer' => 'Bas de page',
+) );
+
+add_theme_support( 'post-thumbnails' ); 
