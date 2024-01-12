abdbcb3c8aaacb0f9832e91ff751b553174f9489
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-01-12 09:49:05 +0100

    Modification affichage informations de contact + header + import font et ajout swiper.js

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
-    <script src="<?php echo  get_template_directory_uri();?>/dist/main.js" defer></script>
+    <script src="<?php echo get_template_directory_uri();?>/dist/main.js" defer></script>
     
     <?php wp_head(); ?>
 </head>
