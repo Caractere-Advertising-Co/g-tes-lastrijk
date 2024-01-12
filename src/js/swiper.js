468855509d311f16866bc2b94008c95eaa7faa0b
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-01-12 14:23:20 +0100

    Début responsive

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -6,6 +6,16 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     spaceBetween: 20,
 
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
