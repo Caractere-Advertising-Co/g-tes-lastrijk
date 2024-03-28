a2f44c35945fd840c45eb4ecba73217498dd007f
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-03-28 15:28:05 +0100

    var_dump

--- templates-parts/blog/card-blog.php
+++ templates-parts/blog/card-blog.php
@@ -16,6 +16,8 @@ if($args['id']):
     $i = $args['id'];
 endif;?>
 
+<?php var_dump($thmb);?>
+
 <?php if($i % 2):?>
     <div class="card-news -left container columns">
         <div class="col-g">
