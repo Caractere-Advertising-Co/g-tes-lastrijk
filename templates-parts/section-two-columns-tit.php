2598d6621eed7da0305651b931fa78503d456144
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-03-29 16:03:44 +0100

    fin correction responsive

--- templates-parts/section-two-columns-tit.php
+++ templates-parts/section-two-columns-tit.php
@@ -11,11 +11,17 @@
         <div class="col-g ">
             <?php if($surtitre): echo '<h3 class="subtitle from-bottom">'.$surtitre.'</h3>';endif;?>
             <?php if($content): echo '<span class="from-bottom">'.$content.'</span>';endif;?>
-            <?php if($cta): echo '<a href="'.$cta['url'].'" class="cta from-bottom">'.$cta['title'].'</a>';endif;?>
+            <?php if($cta): echo '<a href="'.$cta['url'].'" class="cta from-bottom nomobile">'.$cta['title'].'</a>';endif;?>
         </div>
         <div class="col-d ">
             <?php if($img): echo '<div class="img-right"><img src="'.$img['url'].'" alt="'.$img['title'].'" class="from-bottom"></div>';endif;?>
             <?php if($secText): echo '<span class="from-bottom">'.$secText.'</span>';endif;?>
+        </div>
+    </div>
+
+    <div class="nodesktop">
+        <div class="container">
+            <?php if($cta): echo '<a href="'.$cta['url'].'" class="cta from-bottom nodesktop">'.$cta['title'].'</a>';endif;?>
         </div>
     </div>
 </section>
