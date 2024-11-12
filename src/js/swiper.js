f841b7a55686a02e8d5e1fa61bbf5de66abda3dc
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-11-12 12:14:22 +0100

    add space swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -67,18 +67,18 @@ const swiperExtra = new Swiper(".swiper-extra", {
 /* Page Gites */
 
 const swiperGite = new Swiper(".swiper-galerie", {
-  slidesPerView: 5,
-  spaceBetween: 0,
+  slidesPerView: 4,
+  spaceBetween: 30,
   breakpoints: {
     // when window width is >= 320px
     320: {
-      slidesPerView: 2,
-      spaceBetween: 0,
+      slidesPerView: 1.2,
+      spaceBetween: 20,
     },
     // when window width is >= 640px
     640: {
-      slidesPerView: 3,
-      spaceBetween: 50,
+      slidesPerView: 4,
+      spaceBetween: 30,
     },
   },
 })
