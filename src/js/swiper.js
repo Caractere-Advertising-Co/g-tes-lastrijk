a55bf2738228a1025314890341cd783ea49cde87
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-02-05 16:40:02 +0100

    Retrait space betwenne slide

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -45,12 +45,12 @@ const swiperExtra = new Swiper(".swiper-extra", {
     // when window width is >= 320px
     320: {
       slidesPerView: 1.2,
-      spaceBetween: 20,
+      spaceBetween: 0,
     },
     // when window width is >= 640px
     640: {
       slidesPerView: 2,
-      spaceBetween: 20,
+      spaceBetween: 0,
     },
   },
 });
